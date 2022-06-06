f6b4e384468e51a22500c536cf5e60ecc5fe70fe
Mark Hamilton <hamilm25@ferris.edu>
2022-06-06 16:59:48 -0400

    Simulate 10 coin tosses

--- CoinDice/src/CoinDice.java
+++ CoinDice/src/CoinDice.java
@@ -2,13 +2,15 @@
 public class CoinDice {
 
 	public static void main(String[] args) {
-		int side = (int)(Math.random() * 2);
-		if(side == 0)  {
-			System.out.println("Heads");
-		}
-		else  {
-			System.out.println("Tails");
+		for(int i = 0; i < 10; i++) {
+			int side = (int)(Math.random() * 2);
+			if(side == 0)  {
+				System.out.println("Heads");
+			}
+			else  {
+				System.out.println("Tails");
+			}
 		}
 	}
-
+	
 }
